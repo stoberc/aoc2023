d9c473cee2ff1c22373940e1e6d14ece875f8dab
Stober <CHRISTIAN_STOBER@dpsk12.org>
2023-12-01 22:35:03 -0700

    Streamlined Day 2

--- p2.py
+++ p2.py
@@ -1,46 +1,49 @@
-import pdb
+from math import prod
 
 FNAME = "in2.txt"
 
+# parse a draw string like "6 green, 2 red, 5 blue" into rgb values (2, 6, 5) 
 def parse_draw(draw):
+    components = draw.split(',')
+
     result = [0, 0, 0] #rgb
-    for d in draw:
-        if 'red' in d:
-            result[0] += int(d.split()[0])
-        elif 'green' in d:
-            result[1] += int(d.split()[0])
-        elif 'blue' in d:
-            result[2] += int(d.split()[0])
+    for c in components:
+        value = int(c.split()[0])
+        if 'red' in c:
+            result[0] += value
+        elif 'green' in c:
+            result[1] += value
+        elif 'blue' in c:
+            result[2] += value
         else:
-            raise ValueError("Invalid draw:", d);
+            raise ValueError("Invalid draw component:", c);
     return tuple(result)
     
+# break a line apart, returning game number and draw list
 def parse_line(line):
-    gameno = int(line.split()[1][:-1])
     a, b = line.split(": ")
-    draws = b.split("; ")
-    draws = [draw.split(",") for draw in draws]
-    draws = [parse_draw(draw) for draw in draws]
+    gameno = int(a.split()[1])
+    draws = [parse_draw(draw) for draw in b.split("; ")]
     return gameno, draws
     
-def valid1(draw):
+# checks if a draw is valid per the rules of Part 1
+def valid(draw):
     return draw[0] <= 12 and draw[1] <= 13 and draw[2] <= 14
 
-data = [parse_line(line) for line in open(FNAME).read().splitlines()] # in chunks[0]]
+data = [parse_line(line) for line in open(FNAME).read().splitlines()]
 
-result = 0
-for gameno, draws in data:
-    if all(valid1(draw) for draw in draws):
-        result += gameno
+# loop through the data set, summing any valid game
+result = sum(gameno for gameno, draws in data if all(valid(draw) for draw in draws))
         
 print("Part 1:", result)
 
+# find the minimum number of each color necessary for a particular game
+# i.e. the max amount of each color
 def mincubes(draws):
     return [max(i) for i in zip(*draws)]
 
 def power(draw):
-    return draw[0] * draw[1] * draw[2]
+    return prod(draw)
     
-print("Part 2:", sum(power(mincubes(draws)) for _, draws in data))
-
-#pdb.set_trace()
+result = sum(power(mincubes(draws)) for _, draws in data)
+print("Part 2:", result)
