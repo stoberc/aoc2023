ece994b847a449ddc486f359de9f669b6b9998b8
Stober <CHRISTIAN_STOBER@dpsk12.org>
2023-12-02 23:15:37 -0700

    Added DIGITS constant to utils

--- aoc_utils.py
+++ aoc_utils.py
@@ -1,6 +1,8 @@
 DIRECTIONS4 = ((1, 0), (0, 1), (-1, 0), (0, -1))
 DIRECTIONS8 = DIRECTIONS4 + ((1, 1), (-1, -1), (1, -1), (-1, 1))
 
+DIGITS = '0123456789'
+
 # take a collection of points and render them as '#' with non rendered-points as '.'
 # useful for e.g. aoc2021/p13 when a set of points represents a visual message
 def render(points):
