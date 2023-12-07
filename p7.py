8ff9fc808abcb1c070e64160733b79899efb3e59
Stober <CHRISTIAN_STOBER@dpsk12.org>
2023-12-07 08:50:23 -0700

    Refactored Part 1 to use methods from Part 2

--- p7.py
+++ p7.py
@@ -9,24 +9,35 @@ def parse_line(line):
     bid = int(bid)
     return hand, bid
 
+# return a sorted tuple with the counts of cards
+# for example (1, 2, 2) would be two pairs, (2, 3) would be full house...
+def count_profile(hand):
+    cards = set(hand)
+    counts = [hand.count(card) for card in cards]
+    counts.sort()
+    return tuple(counts)
+    
 # return a score 1 to 7 inclusive...
 # 7 is the best (five of a kind)
 # 1 is the worst (high card)
 def classify_hand(hand):
-    if len(set(hand)) == 1: # five of a kind
+    cp = count_profile(hand)
+    if cp == (5,): # five of a kind
         return 7
-    elif len(set(hand)) == 2 and (hand.count(hand[0]) == 1 or hand.count(hand[0]) == 4): # four of a kind
+    elif cp == (1, 4): # four of a kind
         return 6
-    elif len(set(hand)) == 2: # # full house
+    elif cp == (2, 3): # # full house
         return 5
-    elif any(hand.count(i) == 3 for i in hand): # three of a kind
+    elif cp == (1, 1, 3): # three of a kind
         return 4
-    elif len(set(hand)) == 3 and prod(hand.count(i) for i in hand) == 16: # two pair
+    elif cp == (1, 2, 2): # two pair
         return 3
-    elif len(set(hand)) == 4: # single pair
+    elif cp == (1, 1, 1, 2): # single pair
         return 2
     else: # high card
         return 1
+# save the original to allow the updated version for Part 2 to fall back on this when there are no jokers
+classify_hand_classic = classify_hand 
     
 # lookup table converts cards into sequencable values, needed for breaking ties
 VALUE_LUT = {'A':14, 'K':13, 'Q':12, 'J':11, 'T':10, '9':9, '8':8, '7':7, '6':6, '5':5, '4':4, '3':3, '2':2}
@@ -66,46 +77,23 @@ for _, bid in data:
     rank += 1
 print("Part 1:", result)
 
-# return a sorted tuple with the counts of cards
-# for example (1, 2, 2) would be two pairs, (2, 3) would be full house...
-def count_profile(hand):
-    cards = set(hand)
-    counts = [hand.count(card) for card in cards]
-    counts.sort()
-    return tuple(counts)
-    
+
 # redefine this function for Part 2 to deal with jokers
 def classify_hand(hand):
     cp = count_profile(hand)
     if 'J' not in hand: # no jokers
-        if len(set(hand)) == 1: # five of a kind
-            return 7
-        elif len(set(hand)) == 2 and (hand.count(hand[0]) == 1 or hand.count(hand[0]) == 4): # four of a kind
-            return 6
-        elif len(set(hand)) == 2: # # full house
-            return 5
-        elif any(hand.count(i) == 3 for i in hand): # three of a kind
-            return 4
-        elif len(set(hand)) == 3 and prod(hand.count(i) for i in hand) == 16: # two pair
-            return 3
-        elif len(set(hand)) == 4: # single pair
-            return 2
-        else: # high card
-            return 1
-    elif len(set(hand)) <= 2: # 5 of a kind or four of a kind with jokers turns into five of a kind
+        return classify_hand_classic(hand)
+    # any hand w/ at most two distinct cards turns into five-of-a-kind w/ jokers
+    elif cp in [(5,), (1, 4), (2, 3)]: 
         return 7
     elif len(set(hand)) == 3: # three distince values...
-        if hand.count('J') == 3 or hand.count('J') == 2:
+        if cp == (1, 1, 3) or hand.count('J') == 2: # 3 jokers or two-pair w/ two jokers is four-of-a-kind
             return 6
-        elif cp == (1, 1, 3):
-            return 6
-        else:
-            assert cp == (1, 2, 2)
+        else: # two pairs with a lone joker is a full house
             return 5
     elif len(set(hand)) == 4: # four distinct values will always turn into three of a kind
         return 4
     else: # five distinct values will turn into a pair
-        assert len(set(hand)) == 5
         return 2
 
 # reset scoring for jack to be low for tie breakers
@@ -121,4 +109,4 @@ for _, bid in data:
     rank += 1
 print("Part 2:", result)
 
-pdb.set_trace()
+#pdb.set_trace()
