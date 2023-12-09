7d4bc0c709c8aa713be1c257ef6ec85ad2ef0e50
Stober <CHRISTIAN_STOBER@dpsk12.org>
2023-12-08 22:26:28 -0700

    Realized you could just reverse the sequence and use the original function

--- p9.py
+++ p9.py
@@ -6,18 +6,17 @@ def parse_line(line):
     return [int(i) for i in re.findall('-?\d+', line)] # grab all the ints
 data = [parse_line(line) for line in open(FNAME).read().splitlines()]
 
-def extrapolate(sequence, forward = True):
+def extrapolate(sequence):
     # could have base case one layer shallower when all values are ==, but :shrug:
     if all(i == 0 for i in sequence):
         return 0
     diffsequence = []
     for i in range(1, len(sequence)):
         diffsequence.append(sequence[i] - sequence[i - 1])
-    d = extrapolate(diffsequence, forward)
-    if forward:
-        return sequence[-1] + d
-    else:
-        return sequence[0] - d
-
+    d = extrapolate(diffsequence)
+    return sequence[-1] + d
+    
 print("Part 1:", sum(extrapolate(s) for s in data))
-print("Part 2:", sum(extrapolate(s, False) for s in data))
+for s in data:
+    s.reverse()
+print("Part 2:", sum(extrapolate(s) for s in data))
