7529a8ee5314b626d85c8c9c28dc8cb34cceaee2
Stober <CHRISTIAN_STOBER@dpsk12.org>
2023-12-11 22:49:21 -0700

    Renamed variables and fixed comments to match the story

--- p12.py
+++ p12.py
@@ -21,12 +21,12 @@ def count(sequence, values):
             return 1 # valid, but nothing left to fill
         else:
             memo[(sequence, values)] = 0
-            return 0 # impossible to deal with a gear if no values remain
+            return 0 # impossible to deal with a damaged spring if no values remain
     
     # base case - out of sequence
     elif len(sequence) == 0: # and len(vals) > 0
         memo[(sequence, values)] = 0
-        return 0 # out of string, but unsatisfied values :(
+        return 0 # sequence exhausted, but unsatisfied values remain :(
     
     # base case that shortcuts if there's not enough characters left to succeed
     # not needed for correctness but improves runtime
@@ -34,13 +34,13 @@ def count(sequence, values):
         memo[(sequence, values)] = 0
         return 0
    
-    # if the next spot is definitely empty, it's not helpful
+    # if the next spot is definitely operational, it's not helpful
     if sequence[0] == ".":
         result = count(sequence[1:], values)
         memo[(sequence, values)] = result
         return result
         
-    # if it's a gear, it must be part of the leading value
+    # if it's damaged, it must be part of the leading value
     if sequence[0] == "#":
         l = values[0]
         # don't need this if we keep the shortcut that already checked
@@ -49,26 +49,26 @@ def count(sequence, values):
         #    memo[(sequence, values)] = 0
         #    return 0
         # elif...
-        # if there are any empty spots that must be gears, we can't succeed
+        # if there are any operational springs that must be damaged, we can't succeed
         if any(i == "." for i in sequence[:l]):
             memo[(sequence, values)] = 0
             return 0
-        # if the problem continues after this, but the next spot can't be
-        # empty, we can't succeed
+        # if the problem continues after this, but the next spot is damaged
+        # it would prevent the current prefix from being satisfied
         elif len(sequence) > l and sequence[l] == "#":
             memo[(sequence, values)] = 0
             return 0
         # we can satisfy the leading value, so try to satisfy the rest
-        # being sure to skip an empty spot
+        # being sure to skip an operational spring
         result = count(sequence[l + 1:], values[1:])
         memo[(sequence, values)] = result    
         return result
         
     # if it's unknown, we'll try it both ways
     assert sequence[0] == "?"
-    empty_count = count(sequence[1:], values)
-    gear_count = count("#" + sequence[1:], values)
-    result = empty_count + gear_count
+    operational_count = count(sequence[1:], values)
+    damaged_count = count("#" + sequence[1:], values)
+    result = operational_count + damaged_count
     memo[(sequence, values)] = result  
     return result
     
